a44c2cf22fcd9bca8ba8ebf7ce8cf0d92771dee8
Sergio Strati <igesta@strati.it>
2025-05-01 11:58:57 +0200

    maggiori dettagli sulla protezione del file configure.php

--- admin/includes/languages/italiano/modules/security_check/sc_config_file_catalog.php
+++ admin/includes/languages/italiano/modules/security_check/sc_config_file_catalog.php
@@ -10,4 +10,4 @@
   Released under the GNU General Public License
 */
 
-const WARNING_CONFIG_FILE_WRITEABLE = 'Sono abilitato a scrivere nel file di configurazione: ' . DIR_FS_CATALOG . 'includes/configure.php. Quseto è potenzialmente un rischio per il tuo sito - Setta i permessi giusti per questo file.';
+const WARNING_CONFIG_FILE_WRITEABLE = 'Sono abilitato a scrivere nel file di configurazione: ' . DIR_FS_CATALOG . 'includes/configure.php. Quseto è potenzialmente un rischio per il tuo sito - Setta i permessi giusti (in linux: chmod 444 configure.php) per questo file.';
