298829a660cfda0b817294c39e72bdf829646568
Sergio Strati <igesta@strati.it>
2025-06-15 23:32:49 +0200

    San Marino

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 Traduzione in Italiano per Phoenix CE.
 
 Ricordati di adeguare il nome della directory "admin" a quella presente nel tuo negozio
+
+Fai il Login nella sezione Admin
+
+- Vai su Localization
+- Clicca su Languages
+- Clicca su  New Languages
+- Immetti i seguenti Valori:
+-   Name: Italiano
+-   Code: it
+-   Image: icon.gif
+-   Directory: italiano
+-   Sort order: 
+-   Clicca on Save
+
+Per San Marino ci sono solo le zone.
+Per L'Italia ho inseirto anche le aliquote fiscali
+
---
+++ istruzioniSQL_SanMarino.sql
@@ -0,0 +1,9 @@
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'01','Acquaviva');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'02','Chiesanuova');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'03','Domagnano');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'04','Faetano');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'05','Fiorentino');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'06','Borgo Maggiore');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'07','Città di San Marino');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'08','Montegiardino');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (182,'09','Serravalle');
